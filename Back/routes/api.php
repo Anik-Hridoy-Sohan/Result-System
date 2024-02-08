3cbc9e2e6c559288976c6ced6db713f56348e88f
Anik Saha <aniksaha200r@gmail.com>
2024-02-09 01:43:59 +0600

    update

--- Back/routes/api.php
+++ Back/routes/api.php
@@ -22,6 +22,8 @@ use App\Http\Controllers\SemesterController;
 |
 */
 
+// some additional comments
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
